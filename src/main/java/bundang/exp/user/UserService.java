5b04d058e964c120fc919cb2586dd68db3eeaec1
xiah <xiah@rtruesoft.kr>
2019-12-18 16:04:58 +0900

    User 도메인 모델 추가된 것 추가 및 서비스 변경 부분 있음(확인요망)

--- src/main/java/bundang/exp/user/UserService.java
+++ src/main/java/bundang/exp/user/UserService.java
@@ -1,22 +1,71 @@
 package bundang.exp.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import bundang.exp.config.security.JwtTokenProvider;
+import bundang.exp.role.Role;
+import bundang.exp.role.RoleRepository;
+import bundang.exp.role.Roles;
+import bundang.exp.user.dto.JoinDto;
+import bundang.exp.user.dto.LoginDto;
+import bundang.exp.user.security.JwtAuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Service
-public class UserService implements UserDetailsService {
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    BCryptPasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+    private final RoleRepository roleRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findById(username).orElseThrow(() -> new UsernameNotFoundException("user does not exist"));
+    public ResponseEntity<User> join(JoinDto joinDto) {
+        //TODO: 이메일 정규식 테스트
+//        String regExp = "^[a-zA-Z0-9._$%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+//        if(joinDto.getUsername().matches(regExp)) {
+//            new RuntimeException("이메일 형식이 올바르지 않습니다.");
+//        }
+
+        User newUser = modelMapper.map(joinDto, User.class);
+        newUser.setPassword(passwordEncoder.encode(joinDto.getPassword()));
+
+        log.info("role repository = {}", roleRepository.count());
+
+        Role userRole = roleRepository.findByName(Roles.ROLE_USER).orElseThrow(() -> new RuntimeException("User Role not set"));
+        newUser.setRoles(Collections.singletonList(userRole));
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok(newUser);
     }
+
+    public ResponseEntity login(LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
 }
