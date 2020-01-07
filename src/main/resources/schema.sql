9f0765f767c5e5172176c7598f62b61b58f34483
xiah <xiah@rtruesoft.kr>
2020-01-07 15:36:23 +0900

    schema update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,8 +22,6 @@ create table user_roles
 (
 	user_id bigint not null,
 	roles_id bigint not null,
-	constraint UK_amwlmdeik2qdnksxgd566knop
-		unique (roles_id),
 	constraint FK55itppkw3i07do3h7qoclqd4k
 		foreign key (user_id) references user (id),
 	constraint FKdbv8tdyltxa1qjmfnj9oboxse
