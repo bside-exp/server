91c944eec9a906a4959966d02e0bf18611376910
xiah <xiah@rtruesoft.kr>
2020-01-21 13:58:51 +0900

    경험요청 상세보기 로직 추가

--- src/main/java/bundang/exp/controller/ExpRequestController.java
+++ src/main/java/bundang/exp/controller/ExpRequestController.java
@@ -2,16 +2,15 @@ package bundang.exp.controller;
 
 
 import bundang.exp.exp_request.ExpRequestService;
+import bundang.exp.exp_request.domain.ExpRequest;
 import bundang.exp.exp_request.dto.ExpRequestDto;
+import bundang.exp.exp_request.repository.ExpRequestRepository;
 import bundang.exp.user.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,12 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ExpRequestController {
 
+    private final ExpRequestRepository requestRepository;
     private final ExpRequestService service;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpRequest> details(@PathVariable Long id) {
+        ExpRequest expRequest = requestRepository.findById(id).orElseThrow(() -> new RuntimeException("일치하는 게시물을 찾을 수 없습니다."));
+        return ResponseEntity.ok(expRequest);
+    }
+
     @PostMapping
-    private ResponseEntity<ExpRequestDto> create(Authentication authentication, @RequestBody ExpRequestDto expRequestDto) {
+    public ResponseEntity<ExpRequestDto> create(Authentication authentication, @RequestBody ExpRequestDto expRequestDto) {
 
         UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
         return ResponseEntity.ok(service.create(user, expRequestDto));
     }
+
+
 }
